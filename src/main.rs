947930e6120c688fb90838f04c3df55be71f32be
Jordyn <jordyn@husk.pro>
2022-03-15 19:33:10 +0800

    fix: cleanup code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use std::path::Path;
 
 use color_eyre::Result;
-use rusb::{open_device_with_vid_pid, set_log_level};
+use rusb::open_device_with_vid_pid;
 use zkraken_lib::{NZXTDevice, PID, VID};
 
 fn main() -> Result<()> {
-    #[cfg(debug_assertions)]
-    set_log_level(rusb::LogLevel::Debug);
-    
     // We need to use RUSB as well because HIDAPI doesn't support the writing to BULK endpoint.
     let mut handle = open_device_with_vid_pid(VID, PID).expect("No Kraken Z device found!");
     let api = hidapi_rusb::HidApi::new()?;

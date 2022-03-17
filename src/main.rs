8cbdf7becb39ce33e27849cc4fd032ac388d748c
Jordyn <jordyn@husk.pro>
2022-03-17 08:46:32 +0800

    refactor: cleanup code

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,14 @@ use rusb::open_device_with_vid_pid;
 use zkraken_lib::{NZXTDevice, PID, VID};
 
 fn main() -> Result<()> {
-    // We need to use RUSB as well because HIDAPI doesn't support the writing to BULK endpoint.
     let mut handle = open_device_with_vid_pid(VID, PID).expect("No Kraken Z device found!");
-    let api = hidapi_rusb::HidApi::new()?;
-    let hid_device = api.open(VID, PID)?;
-    let nzxt_device = NZXTDevice::new(&hid_device, &mut handle, 270)?;
+    let nzxt_device = NZXTDevice::new(&mut handle, 270)?;
+
+    let status = nzxt_device.get_status()?;
+    let firmware_version = nzxt_device.get_firmware_version()?;
+
+    println!("Status: {:?}", status);
+    println!("Firmware version: {}", firmware_version);
 
     nzxt_device.set_fan_duty(80)?;
     nzxt_device.set_pump_duty(80)?;
